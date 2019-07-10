89adde900537f8f7a6c23824ee9ae75c4149b958
Stanley Sham <stanley.sham94@gmail.com>
2019-07-10 11:43:37 -0400

    completed exercise 5

--- exercise7-5.py
+++ exercise7-5.py
@@ -1,7 +1,7 @@
 def convert_to_c(fahrenheit):
     fahrenheit = int(fahrenheit)
     c = (fahrenheit-32) * (5/9)
-    return "{} Fahrenheit is {:.2f} Celcius".format(fahrenheit, c)
+    return "{} Fahrenheit is {:.2f} Celcius".format(fahrenheit, c) # Making sure that celcius only comes out in two decimal places
 
 print(convert_to_c(79))
 print(convert_to_c(60))
