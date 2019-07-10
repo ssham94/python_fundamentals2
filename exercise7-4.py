942493d36e92849965de29f75b1a76e778da19b5
Stanley Sham <stanley.sham94@gmail.com>
2019-07-10 11:40:26 -0400

    completed exercise 4

--- exercise7-4.py
+++ exercise7-4.py
@@ -6,6 +6,6 @@ def long_word(my_word):
 
 print(long_word("helloworld"))
 print(long_word("hello"))
-print(long_word("im hungry")) #It includes the space
+print(long_word("im hungry")) #It includes the space between words
 print(long_word("ardvark"))
 print(long_word("timetoeat"))
